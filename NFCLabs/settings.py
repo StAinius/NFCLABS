65039bcb9091546dceb496e41778a1dfb28c771d
StAinius <stonkusainius@gmail.com>
2025-05-28 13:55:59 +0300

    Pakeitimai

--- NFCLabs/settings.py
+++ NFCLabs/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = "RENDER" not in os.environ
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/nfclabs_media'
+MEDIA_ROOT = '/app/media/'
 
 SECRET_KEY = "django-insecure-#jtq0thhz2k8z^(jwth*f4aafddr^tq5zgyzbf1j2r)zr(galw"
 
--- website_content/admin.py
+++ website_content/admin.py
@@ -4,30 +4,26 @@ from .models import PageContent
 from tinymce.widgets import TinyMCE
 from django.conf import settings
 
-
 @admin.register(PageContent)
 class PageContentAdmin(admin.ModelAdmin):
-    list_display = ("get_display_name", "title", "updated_at")
-    search_fields = ("content",)
-    readonly_fields = ("page_key",)
-
-    def formfield_for_dbfield(self, db_field, request, **kwargs):
-        if db_field.name == "content":
-            kwargs["widget"] = TinyMCE(
-                attrs={"cols": 80, "rows": 30},
-                mce_attrs=settings.TINYMCE_DEFAULT_CONFIG,
-            )
-        return super().formfield_for_dbfield(db_field, request, **kwargs)
-
+    list_display = ('get_display_name', 'title', 'updated_at')
+    search_fields = ('content',)
+    readonly_fields = ('page_key',)
+    
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 30},
+                                          mce_attrs=settings.TINYMCE_DEFAULT_CONFIG)},
+    }
+    
     def get_display_name(self, obj):
         return obj.get_display_name()
-
-    get_display_name.short_description = "Website"
-
+    get_display_name.short_description = 'Website'
+    
     def has_delete_permission(self, request, obj=None):
         return False
-
+    
     def has_add_permission(self, request):
         count = PageContent.objects.count()
         max_pages = len(PageContent.WEBSITES)
         return count < max_pages
+    

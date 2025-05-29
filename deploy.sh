090371d14bf21b81966f15b3292664d6ab3c872e
StAinius <stonkusainius@gmail.com>
2025-05-29 12:33:37 +0300

    Add execute permission to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-
 git add .
-git stash
+git stash  
 git fetch origin main
 git reset --hard origin/main
-
-docker compose -f docker-compose.production.yml down
-
+docker compose -f docker-compose.production.yml stop
 docker compose -f docker-compose.production.yml up -d --build
-
 sleep 10
-
 docker compose -f docker-compose.production.yml exec web python manage.py collectstatic --noinput
-
 if docker compose -f docker-compose.production.yml ps | grep -q "Up"; then
     docker compose -f docker-compose.production.yml ps
 else
     docker compose -f docker-compose.production.yml logs
 fi
-
 echo "Done!"

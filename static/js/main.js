50fc25892d18b786d50b566d6de05bfacb1cee11
StAinius <stonkusainius@gmail.com>
2025-05-30 16:10:58 +0300

    Pakeitimai

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,31 @@
-setTimeout(function() {
+setTimeout(function () {
     var messageElement = document.getElementById('success-message');
-    if (messageElement) { 
+    if (messageElement) {
         messageElement.style.transition = 'opacity 0.5s ease';
         messageElement.style.opacity = '0';
-            
-    setTimeout(function() {
-        messageElement.remove();
+
+        setTimeout(function () {
+            messageElement.remove();
         }, 500);
     }
 }, 5000);
+
+
+// Our products recommendations
+const mobileProductCards = document.querySelectorAll('.mobile-product-card');
+
+mobileProductCards.forEach(card => {
+    card.addEventListener('click', function (e) {
+        if (e.target.closest('a')) {
+            return;
+        }
+
+        this.classList.toggle('active');
+
+        mobileProductCards.forEach(otherCard => {
+            if (otherCard !== this) {
+                otherCard.classList.remove('active');
+            }
+        });
+    });
+});
